c39252dd6db08dd031ce00e00e93a028501afe56
LunaMorena9 <melkamar7@gmail.com>
2020-08-01 18:09:14 +0200

    upp

--- models/book.js
+++ models/book.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
-const { isDate } = require('lodash')
-
+const path = require('path')
+const coverImageBasePath = 'uploads/bookCovers'
 const bookSchema = new mongoose.Schema({
     title: {
         type: String,
@@ -18,7 +18,7 @@ const bookSchema = new mongoose.Schema({
         required: true
     },
     createdAt: {
-        type: String,
+        type: Date,
         required: true,
         default: Date.now
     },
@@ -33,5 +33,11 @@ const bookSchema = new mongoose.Schema({
     }
 })
 
+bookSchema.virtual('coverImagePath').get(function(){
+    if (this.coverImageName != null) {
+        return path.join('/', coverImageBasePath, this.coverImageName)
+    }
+})
 
 module.exports = mongoose.model('Book', bookSchema)
+module.exports.coverImageBasePath = coverImageBasePath
